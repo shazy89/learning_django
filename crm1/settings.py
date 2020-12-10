4fa773574caff7e12635f286134c487823ef43b7
shazy89 <erdo.shazy123@gmail.com>
2020-12-10 11:08:51 -0500

    django queries

--- crm1/settings.py
+++ crm1/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'accounts',
+    'accounts', # tell django you're going to use those models
 ]
 
 MIDDLEWARE = [
