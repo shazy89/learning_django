4182a601f412f5be7dce306e3cc0c9d57c43ec3f
shazy89 <erdo.shazy123@gmail.com>
2020-12-10 11:36:06 -0500

    models and render data to templates

--- accounts/models.py
+++ accounts/models.py
@@ -8,7 +8,6 @@ class Customer(models.Model):
 	email = models.CharField(max_length=200, null=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 
-
 	def __str__(self):
 		return self.name
 
@@ -28,15 +27,12 @@ class Product(models.Model):
 	name = models.CharField(max_length=200, null=True)
 	price = models.FloatField(null=True)
 	category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-	description = models.CharField(max_length=200, null=True, blank=True) #blank=true can be empty
+	description = models.CharField(max_length=200, null=True, blank=True)
 	date_created = models.DateTimeField(auto_now_add=True, null=True)
 	tags = models.ManyToManyField(Tag)
 
 	def __str__(self):
 		return self.name
-
-
-
 
 class Order(models.Model):
 	STATUS = (
--- accounts/views.py
+++ accounts/views.py
@@ -1,20 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+from django.http import HttpResponse
 # Create your views here.
+from .models import *
 
-#def home(request): for response
-#     return HttpResponse('home')
-def home(request): 
-    return render(request, 'accounts/dashboard.html')
+
+def home(request):
+	orders = Order.objects.all()
+	customers = Customer.objects.all()
+
+	total_customers = customers.count()
+
+	total_orders = orders.count()
+	delivered = orders.filter(status='Delivered').count()
+	pending = orders.filter(status='Pending').count()
+
+	context = {'orders':orders, 'customers':customers,
+	'total_orders':total_orders,'delivered':delivered,
+	'pending':pending }
+
+	return render(request, 'accounts/dashboard.html', context)
+
 def products(request):
-    return render(request, 'accounts/products.html')
+	products = Product.objects.all()
+
+	return render(request, 'accounts/products.html', {'products':products})
+
 def customer(request):
-    return render(request, 'accounts/customer.html')
-
-
-# -- accounts
-# ---- templates
-# ---- accounts
-# ----- dashboard.html
-# -----profile.html
-# -----customer.html
+	return render(request, 'accounts/customer.html')
